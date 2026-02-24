43703fdc9127867b03937ecec6b05f8b7e3c6003
Will Moore <will@clevercode.ca>
2026-02-23 16:00:38 -0800

    correct whisper

--- core/config.py
+++ core/config.py
@@ -17,7 +17,7 @@ PIPER_CMD = "./piper/piper"
 PIPER_MODEL = "./piper/en_GB-semaine-medium.onnx"
 
 # STT Settings (Whisper.cpp)
-WHISPER_CMD = "./whisper.cpp/main"
+WHISPER_CMD = "./whisper.cpp/build/bin/whisper-cli"
 WHISPER_MODEL = "./whisper.cpp/models/ggml-base.en.bin"
 
 # Audio Settings
